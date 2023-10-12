fc2472e03b296ca1be1cdfe6fc966d471789f35e
deverte <deverte@ya.ru>
2023-10-12 17:12:21 +0700

    2023-10-12

--- exo3d_tools/plot_1.py
+++ exo3d_tools/plot_1.py
@@ -10,10 +10,11 @@ def _plot_1(
     array: "np.ndarray[(2 * (nR + 1))]",
     grid: np.ndarray,
     ax: "matplotlib.axes._axes.Axes" = None,
+    **kwargs,
 ) -> "matplotlib.axes._axes.Axes":
     if not ax:
         import matplotlib.pyplot as plt
         fig = plt.figure()
         ax = fig.add_subplot(111)
-    ax.plot(grid, array)
+    ax.plot(grid, array, **kwargs)
     return ax
--- exo3d_tools/plot_2.py
+++ exo3d_tools/plot_2.py
@@ -10,7 +10,7 @@ def _plot_2_cartesian(
     array: "np.ndarray[(2 * (nR + 1))]",
     grid: np.ndarray,
     ax: "matplotlib.axes._axes.Axes" = None,
-    shading: typing.Literal["auto", "gouraud", "nearest"] = "auto",
+    **kwargs,
 ) -> "matplotlib.axes._axes.Axes":
     if not ax:
         import matplotlib.pyplot as plt
@@ -18,7 +18,7 @@ def _plot_2_cartesian(
         ax = fig.add_subplot(111)
     x = grid.n[1]
     y = grid.n[0]
-    ax.pcolormesh(x, y, array.swapaxes(0, 1), shading=shading)
+    ax.pcolormesh(x, y, array.swapaxes(0, 1), **kwargs)
     return ax
 
 
@@ -26,7 +26,7 @@ def _plot_2_polar(
     array: "np.ndarray[(2 * (nR + 1))]",
     grid: np.ndarray,
     ax: "matplotlib.axes._axes.Axes" = None,
-    shading: typing.Literal["auto", "gouraud", "nearest"] = "auto",
+    **kwargs,
 ) -> "matplotlib.axes._axes.Axes":
     if not ax:
         import matplotlib.pyplot as plt
@@ -34,5 +34,5 @@ def _plot_2_polar(
         ax = fig.add_subplot(111, projection="polar")
     r = grid.n[1]
     phi = grid.n[0]
-    ax.pcolor(phi, r, array.swapaxes(0, 1), shading=shading)
+    ax.pcolor(phi, r, array.swapaxes(0, 1), **kwargs)
     return ax
